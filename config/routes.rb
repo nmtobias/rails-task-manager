8ce724553664d5a5dc117001f37abf617dc94de5
nuno tobias <nmtobias@gmail.com>
2020-01-28 21:26:26 +0000

    added list and show

--- config/routes.rb
+++ config/routes.rb
@@ -5,17 +5,17 @@ Rails.application.routes.draw do
   get 'tasks', to: 'tasks#index'
 
   # 4 - As a user, I can view the details of a task
-  get 'tasks/id', to: 'tasks#show'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
   # 5 - As a user, I can add a new task (2 requests)
   # 1 to get the empty form
   get 'tasks/new', to: 'tasks#new'
   # 2 to submit the form
   post 'tasks', to: 'tasks#create'
   # 6 - As a user, I can edit a task
-  get 'tasks/id/edit', to: 'tasks#edit'
+  get 'tasks/:id/edit', to: 'tasks#edit'
 
-  patch 'task/id', to: 'tasks#update'
+  patch 'task/:id', to: 'tasks#update'
   # 7 - As a user, I can remove a task
-  delete 'tasks/id', to: 'tasks#destroy'
+  delete 'tasks/:id', to: 'tasks#destroy'
 
 end
